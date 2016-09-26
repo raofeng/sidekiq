043d5268526a928ea8d1f92ef1e0a2e5c4667b7f
Ray Rao <raofeng@live.com>
2016-09-26 14:08:46 +0800

    add redis namespace

--- config.rb
+++ config.rb
@@ -1,13 +1,13 @@
 config_env :development do
   set 'URL', 'redis://127.0.0.1:6379/1'
-  set 'NAMESPACE', 'hello'
+  set 'NAMESPACE', 'HELLOWORLD'
   set 'USERNAME', 'admin'
   set 'PASSWORD', '123456'
 end
 
 config_env :production do
-  set 'URL', 'redis://@127.0.0.1:6379/1'
-  set 'NAMESPACE', 'world'
+  set 'URL', 'redis://:NEHkyMGhkD1d@127.0.0.1:6379/1'
+  set 'NAMESPACE', 'HELLOWORLD'
   set 'USERNAME', 'admin'
   set 'PASSWORD', '123456'
 end
